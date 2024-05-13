135e38386130fe23eb2adba8aeaea6ad192f37b6
Jem <0x0xjem@gmail.com>
2024-05-13 10:33:41 +0400

    Stub for the create auction guide

---
+++ script/guides/01-create-auction.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+import {IAuctionHouse} from "src/interfaces/IAuctionHouse.sol";
+
+contract CreateAuctionScript is Script {
+    function run() external {
+        // Define the deployed AuctionHouse
+
+        // Define the tokens used in the auction
+
+        // Define the auction routing parameters
+
+        // Define the auction module parameters
+
+        // Define the auction parameters
+
+        // Create the auction
+    }
+}
