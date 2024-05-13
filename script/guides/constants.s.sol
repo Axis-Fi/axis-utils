e8a115bd3b901b034597683208111e37bc61096d
Jem <0x0xjem@gmail.com>
2024-05-13 11:56:10 +0400

    Create auction script

--- script/guides/constants.s.sol
+++ script/guides/constants.s.sol
@@ -4,4 +4,10 @@ pragma solidity ^0.8.19;
 abstract contract Constants {
     /// @dev This is a testnet address
     address internal _auctionHouse = address(0xAA0000A0F9FF55d5F00aB9cc8d05eF78DE4f9E8f);
+
+    address internal constant _OWNER = address(0x1);
+    address internal constant _SELLER = address(0x2);
+    address internal constant _PROTOCOL = address(0x3);
+    address internal constant _REFERRER = address(0x4);
+    address internal constant _BUYER = address(0x5);
 }
--- script/guides/create-auction.s.sol
+++ script/guides/create-auction.s.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity >=0.8.0;
 
+// Script setup
+import {console2} from "forge-std/console2.sol";
 import {Script} from "forge-std/Script.sol";
-
-import {IAuctionHouse} from "src/interfaces/IAuctionHouse.sol";
-
 import {Constants} from "script/guides/constants.s.sol";
+
+// Mocks
+import {MockERC20} from "src/lib/MockERC20.sol";
+
+// Axis contracts
+import {IAuctionHouse} from "src/interfaces/IAuctionHouse.sol";
+import {IAuction} from "src/interfaces/IAuction.sol";
+import {ICallback} from "src/interfaces/ICallback.sol";
+import {toKeycode} from "src/modules/Keycode.sol";
+import {IEncryptedMarginalPrice} from "src/interfaces/modules/auctions/IEncryptedMarginalPrice.sol";
 
 contract CreateAuctionScript is Script, Constants {
     function run() external {
@@ -13,13 +22,59 @@ contract CreateAuctionScript is Script, Constants {
         IAuctionHouse auctionHouse = IAuctionHouse(_auctionHouse);
 
         // Define the tokens used in the auction
+        MockERC20 quoteToken = new MockERC20();
+        quoteToken.initialize("Quote Token", "QT", 18);
+        MockERC20 baseToken = new MockERC20();
+        baseToken.initialize("Base Token", "BT", 18);
 
         // Define the auction routing parameters
+        IAuctionHouse.RoutingParams memory routingParams = IAuctionHouse.RoutingParams({
+            auctionType: toKeycode("EMPA"),
+            baseToken: address(baseToken),
+            quoteToken: address(quoteToken),
+            curator: address(0),
+            callbacks: ICallback(address(0)),
+            callbackData: abi.encode(""),
+            derivativeType: toKeycode(""),
+            derivativeParams: abi.encode(""),
+            wrapDerivative: false
+        });
 
         // Define the auction module parameters
+        IEncryptedMarginalPrice.AuctionDataParams memory empParams = IEncryptedMarginalPrice
+            .AuctionDataParams({
+            minPrice: 1e18,
+            minFillPercent: 10_000,
+            minBidSize: 1e18,
+            publicKey: IEncryptedMarginalPrice.Point({x: 0, y: 0})
+        });
 
         // Define the auction parameters
+        uint48 start = uint48(block.timestamp + 1 days);
+        uint48 duration = uint48(3 days);
+        uint256 capacity = 10e18;
+
+        IAuction.AuctionParams memory auctionParams = IAuction.AuctionParams({
+            start: start,
+            duration: duration,
+            capacityInQuote: false,
+            capacity: capacity,
+            implParams: abi.encode(empParams)
+        });
+
+        // Mint base tokens to the seller
+        baseToken.mint(_SELLER, 10e18);
+
+        // The AuctionHouse will pull base tokens from the seller upon auction creation,
+        // so approve the auction capacity
+        vm.prank(_SELLER);
+        baseToken.approve(address(auctionHouse), 10e18);
+
+        // Define the IPFS hash for additional information
+        string memory ipfsHash = "";
 
         // Create the auction
+        uint96 lotId = auctionHouse.auction(routingParams, auctionParams, ipfsHash);
+        console2.log("Created auction with lot ID:", lotId);
     }
 }
